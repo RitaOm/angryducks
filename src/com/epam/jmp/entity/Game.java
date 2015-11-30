5d3be0d718781e382493a66688ca123eba81ff44
Rita <omelyanchuk.rita@mail.ru>
2015-11-30 10:06:47 +0300

    partially finished

--- src/com/epam/jmp/entity/Game.java
+++ src/com/epam/jmp/entity/Game.java
@@ -1,5 +1,13 @@
 package com.epam.jmp.entity;
 
+import java.util.List;
+
+import com.epam.jmp.entity.duck.Duck;
+import com.epam.jmp.entity.labyrinth.Labyrinth;
+
 public class Game {
 
+	public Game(List<Duck> players, Labyrinth labyrinth) {
+
+	}
 }
